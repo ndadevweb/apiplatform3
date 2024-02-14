5f55c8b9b5cd26f18665dd60632ac678de165d79
Nicolas D <ndadevweb>
2024-02-14 20:54:42 +0100

    Testing Authentication

--- tests/functional/DragonTreasureResourceTest.php
+++ tests/functional/DragonTreasureResourceTest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Functional;
 
 use App\Factory\DragonTreasureFactory;
+use App\Factory\UserFactory;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Zenstruck\Browser\Json;
 use Zenstruck\Browser\Test\HasBrowser;
@@ -36,5 +37,30 @@ class DragonTreasureResourceTest extends KernelTestCase
             'shortDescription',
             'plunderedAtAgo',
         ]);
+    }
+
+    public function testPostToCreateTreasure(): void
+    {
+        $user = UserFactory::createOne();
+
+        $this->browser()
+            ->actingAs($user)
+            ->post('/api/treasures', [
+                'json' => [],
+            ])
+            ->assertStatus(422)
+            ->post('/api/treasures', [
+                'json' => [
+                    'name' => 'A shiny thing',
+                    'description' => 'It sparkles when I wave it in the air.',
+                    'value' => 1000,
+                    'coolFactor' => 5,
+                    'owner' => '/api/users/'.$user->getId(),
+                ],
+            ])
+            ->assertStatus(201)
+            ->dump()
+            ->assertJsonMatches('name', 'A shiny thing')
+        ;
     }
 }
