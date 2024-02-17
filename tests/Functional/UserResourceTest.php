40cb7676c5911c5c883df50754bf59e24062d73b
Nicolas D <ndadevweb@gmail.com>
2024-02-17 15:53:43 +0100

    Validation Groups & Patch Formats

--- tests/Functional/UserResourceTest.php
+++ tests/Functional/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Functional;
 
+use App\Factory\UserFactory;
 use Zenstruck\Foundry\Test\ResetDatabase;
 
 class UserResourceTest extends ApiTestCase
@@ -26,6 +27,24 @@ class UserResourceTest extends ApiTestCase
                 ]
             ])
             ->assertSuccessful()
+        ;
+    }
+
+    public function testPatchToUpdateUser(): void
+    {
+        $user = UserFactory::createOne();
+
+        $this->browser()
+            ->actingAs($user)
+            ->patch('/api/users/'.$user->getId(), [
+                'json' => [
+                    'username' => 'changed',
+                ],
+                'headers' => [
+                    'Content-Type' => 'application/merge-patch+json'
+                ]
+            ])
+            ->assertStatus(200)
         ;
     }
 }
