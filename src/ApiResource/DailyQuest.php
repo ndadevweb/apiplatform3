0f679ab698d7c3810f0dbfb35e65365883226fb8
Nicolas D <ndadevweb@gmail.com>
2024-02-21 00:07:26 +0100

    Using a Custom (Date) Identifier

--- src/ApiResource/DailyQuest.php
+++ src/ApiResource/DailyQuest.php
@@ -2,10 +2,37 @@
 
 namespace App\ApiResource;
 
+use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use App\Enum\DailyQuestStatusEnum;
+use App\State\DailyQuestStateProvider;
+use Symfony\Component\Serializer\Annotation\Ignore;
 
-#[ApiResource()]
+#[ApiResource(
+    shortName: 'Quest',
+    provider: DailyQuestStateProvider::class
+)]
 class DailyQuest
 {
+    #[Ignore]
+    public \DateTimeInterface $day;
 
+    public string $questName;
+
+    public string $description;
+
+    public int $difficultyLevel;
+
+    public DailyQuestStatusEnum $status;
+
+    public function __construct(\DateTimeInterface $day)
+    {
+        $this->day = $day;
+    }
+
+    #[ApiProperty(readable: false, identifier: true)]
+    public function getDayString(): string
+    {
+        return $this->day->format('Y-m-d');
+    }
 }
