f3175e0f164849308a97978a20d1d63d8af4c1ce
Nicolas D <ndadevweb@gmail.com>
2024-02-21 20:53:46 +0100

    stateOptions + entityClass Magic

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -2,12 +2,30 @@
 
 namespace App\ApiResource;
 
+use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Doctrine\Orm\State\CollectionProvider;
+use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiResource;
+use App\Entity\User;
 
 #[ApiResource(
     shortName: 'User',
+    paginationItemsPerPage: 5,
+    stateOptions: new Options(entityClass: User::class)
 )]
+#[ApiFilter(SearchFilter::class, properties: [
+    'username' => 'partial'
+])]
 class UserApi
 {
     public ?int $id = null;
+
+    public ?string $email = null;
+
+    public ?string $username = null;
+
+    /**
+     * @var array<int, \App\Entity\DragonTreasure>
+     */
+    public array $dragonTreasures = [];
 }
