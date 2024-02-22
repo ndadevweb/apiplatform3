f83f13b689b69730a34aec7c56516709e5a00908
Nicolas D <ndadevweb@gmail.com>
2024-02-22 13:05:16 +0100

    Leveraging the Core Processor

--- src/State/EntityClassDtoStateProcessor.php
+++ src/State/EntityClassDtoStateProcessor.php
@@ -2,26 +2,45 @@
 
 namespace App\State;
 
+use ApiPlatform\Doctrine\Common\State\PersistProcessor;
+use ApiPlatform\Doctrine\Common\State\RemoveProcessor;
+use ApiPlatform\Metadata\DeleteOperationInterface;
 use ApiPlatform\Metadata\Operation;
 use ApiPlatform\State\ProcessorInterface;
 use App\ApiResource\UserApi;
 use App\Entity\User;
 use App\Repository\UserRepository;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class EntityClassDtoStateProcessor implements ProcessorInterface
 {
     public function __construct(
-        private UserRepository $userRepository
+        private UserRepository $userRepository,
+        #[Autowire(service: PersistProcessor::class)] private ProcessorInterface $persistProcessor,
+        #[Autowire(service: RemoveProcessor::class)] private ProcessorInterface $removeProcessor,
+        private UserPasswordHasherInterface $userPasswordHasher
     )
     {
 
     }
 
-    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = [])
     {
         assert($data instanceof UserApi);
     
         $entity = $this->mapDtoToEntity($data);
+
+        if($operation instanceof DeleteOperationInterface) {
+            $this->removeProcessor->process($entity, $operation, $uriVariables, $context);
+
+            return null;
+        }
+
+        $this->persistProcessor->process($entity, $operation, $uriVariables, $context);
+        $data->id = $entity->getId();
+
+        return $data;
     }
 
     private function mapDtoToEntity(object $dto): object
@@ -42,6 +61,9 @@ class EntityClassDtoStateProcessor implements ProcessorInterface
         $entity->setUsername($dto->username);
         $entity->setPassword('TODO properly');
 
+        if($dto->password) {
+            $entity->setPassword($this->userPasswordHasher->hashPassword($entity, $dto->password));
+        }
         // todo dandle dragon treasure
 
         return $entity;
