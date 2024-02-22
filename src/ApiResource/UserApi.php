a5bb2352234547b1be97b49129c8171db9583067
Nicolas D <ndadevweb@gmail.com>
2024-02-22 21:58:15 +0100

    DTO Validation & Security

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -7,13 +7,29 @@ use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiProperty;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\{Get, GetCollection, Post, Patch, Delete};
 use App\Entity\User;
 use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 #[ApiResource(
     shortName: 'User',
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            security: 'is_granted("PUBLIC_ACCESS")',
+            validationContext: ['groups' => ['Default', 'postValidation']]
+        ),
+        new Patch(
+            security: 'is_granted("ROLE_USER_EDIT")'
+        ),
+        new Delete()
+    ],
     paginationItemsPerPage: 5,
+    security: 'is_granted("ROLE_USER")',
     provider: EntityToDtoStateProvider::class,
     processor: EntityClassDtoStateProcessor::class,
     stateOptions: new Options(entityClass: User::class),
@@ -26,14 +42,18 @@ class UserApi
     #[ApiProperty(readable: false, writable: false, identifier: true)]
     public ?int $id = null;
 
+    #[NotBlank()]
+    #[Email()]
     public ?string $email = null;
 
+    #[NotBlank()]
     public ?string $username = null;
 
     /**
      * The plaintext password when being set or changed.
      */
     #[ApiProperty(readable: false)]
+    #[NotBlank(groups: ['postValidation'])]
     public ?string $password = null;
 
     /**
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -64,8 +64,8 @@ use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 //     ]
 // )]
 // #[ApiFilter(PropertyFilter::class)]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
-#[UniqueEntity(fields: ['username'], message: 'It looks like another dragon took your username. ROAR!')]
+// #[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+// #[UniqueEntity(fields: ['username'], message: 'It looks like another dragon took your username. ROAR!')]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
