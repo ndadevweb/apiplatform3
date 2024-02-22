74d73ef87ac27f3937e5e0440c064aed4c3a76e1
Nicolas D <ndadevweb@gmail.com>
2024-02-22 12:47:59 +0100

    DTO -> Entity State Processor

--- src/ApiResource/UserApi.php
+++ src/ApiResource/UserApi.php
@@ -8,12 +8,14 @@ use ApiPlatform\Doctrine\Orm\State\Options;
 use ApiPlatform\Metadata\ApiFilter;
 use ApiPlatform\Metadata\ApiResource;
 use App\Entity\User;
+use App\State\EntityClassDtoStateProcessor;
 use App\State\EntityToDtoStateProvider;
 
 #[ApiResource(
     shortName: 'User',
     paginationItemsPerPage: 5,
     provider: EntityToDtoStateProvider::class,
+    processor: EntityClassDtoStateProcessor::class,
     stateOptions: new Options(entityClass: User::class)
 )]
 #[ApiFilter(SearchFilter::class, properties: [
---
+++ src/State/EntityClassDtoStateProcessor.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\State;
+
+use ApiPlatform\Metadata\Operation;
+use ApiPlatform\State\ProcessorInterface;
+use App\ApiResource\UserApi;
+use App\Entity\User;
+use App\Repository\UserRepository;
+
+class EntityClassDtoStateProcessor implements ProcessorInterface
+{
+    public function __construct(
+        private UserRepository $userRepository
+    )
+    {
+
+    }
+
+    public function process(mixed $data, Operation $operation, array $uriVariables = [], array $context = []): void
+    {
+        assert($data instanceof UserApi);
+    
+        $entity = $this->mapDtoToEntity($data);
+    }
+
+    private function mapDtoToEntity(object $dto): object
+    {
+        assert($dto instanceof UserApi);
+
+        if($dto->id) {
+            $entity = $this->userRepository->find($dto->id);
+
+            if(!$entity) {
+                throw new \Exception(sprintf('Entity %d not found', $dto->id));
+            }
+        } else {
+            $entity = new User();
+        }
+
+        $entity->setEmail($dto->email);
+        $entity->setUsername($dto->username);
+        $entity->setPassword('TODO properly');
+
+        // todo dandle dragon treasure
+
+        return $entity;
+    }
+}
